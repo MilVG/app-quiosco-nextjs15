39d2aaf8723dd7e676e158677e953c048201e2b4
notlim_dev <elmotigre6@gmail.com>
2024-11-14 11:59:31 -0500

    Obtener los productos mediante su id - service prisma- Data_Fetching

--- src/app/order/[category]/page.tsx
+++ src/app/order/[category]/page.tsx
@@ -1,4 +1,20 @@
-export default function OrderPage() {
+import { prisma } from "@/lib/prisma"
+
+async function getProducts(category: string) {
+
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  })
+  return products
+}
+export default async function OrderPage({ params }: { params: { category: string } }) {
+  const products = await getProducts(params.category)
+  console.log(products);
+
   return (
     <div>OrderPage</div>
   )
