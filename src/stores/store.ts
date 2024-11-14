720618669beb032757fe271e798c34e2d74beb8f
notlim_dev <elmotigre6@gmail.com>
2024-11-14 18:57:12 -0500

    Implementamos y agregamos en nuestro store los eventos de incrementar ordenes y eliminarlas

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -6,6 +6,8 @@ interface Store {
   order: OrderItem[]
   addToCard: (product: Product) => void
   increaseQuantity: (id: Product['id']) => void
+  decreaseQuantity: (id: Product['id']) => void
+  removeItem: (id: Product['id']) => void
 }
 export const useStoreQuiosco = create<Store>((set, get) => ({
   order: [],
@@ -41,6 +43,22 @@ export const useStoreQuiosco = create<Store>((set, get) => ({
         quantity: item.quantity + 1,
         subtotal: item.price * (item.quantity + 1)
       } : item)
+    }))
+  },
+  decreaseQuantity: (id) => {
+    const order = get().order.map(item => item.id === id ? {
+      ...item,
+      quantity: item.quantity - 1,
+      subtotal: item.price * (item.quantity - 1)
+    } : item)
+    set(() => ({
+      order
+    }))
+  },
+  removeItem: (id) => {
+
+    set((state) => ({
+      order: state.order.filter(item => item.id !== id)
     }))
   }
 }))
