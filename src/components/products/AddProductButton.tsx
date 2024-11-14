5cf3d55b27ff1526fb609d40713580145d0cab9f
notlim_dev <elmotigre6@gmail.com>
2024-11-14 15:37:55 -0500

    transformamos el compoenente del tipo cliente - para manejar el state de la orden

---
+++ src/components/products/AddProductButton.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Product } from "@prisma/client"
+import { useStoreQuiosco } from "@/stores/store"
+
+type AddProductButtonProps = {
+  product: Product
+}
+export default function AddProductButton({ product }: AddProductButtonProps) {
+
+  const addTocard = useStoreQuiosco((state) => state.addToCard)
+  return (
+    <button
+      type="button"
+      className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3
+          uppercase font-bold cursor-pointer"
+      onClick={() => addTocard(product)}
+    >Agregar</button>
+  )
+}
+
