2404a289de4c8548bfa475bbfd50998bfa793a3a
notlim_dev <elmotigre6@gmail.com>
2024-11-14 11:41:49 -0500

    Finalizacion de capitulo 3 - categoria platillos -- Data_fetching y Routing-Dinamic

--- src/components/OrderSidebar.tsx
+++ src/components/OrderSidebar.tsx
@@ -1,9 +1,26 @@
-export default function OrderSidebar() {
+import { prisma } from "@/lib/prisma"
+import CategoryIcon from "./ui/CategoryIcon"
+
+async function getCategorires() {
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+
+  const categorires = await getCategorires()
+
   return (
     <aside
       className="md:w-72 md:h-screen bg-white"
     >
-      OrderSidebar
+      <nav className="mt-10">
+        {categorires.map(category => (
+          <CategoryIcon
+            key={category.id}
+            category={category}
+          />
+        ))}
+      </nav>
     </aside>
   )
 }
