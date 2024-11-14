6639ce9c04af2d12b2db695ff38fbc7df9c50908
notlim_dev <elmotigre6@gmail.com>
2024-11-14 13:12:16 -0500

    Recorremos el array y enviamos los datos al card de Productos

--- src/app/order/[category]/page.tsx
+++ src/app/order/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/products/ProductCard";
 import { prisma } from "@/lib/prisma"
 
 async function getProducts(category: string) {
@@ -13,10 +14,20 @@ async function getProducts(category: string) {
 }
 export default async function OrderPage({ params }: { params: { category: string } }) {
   const products = await getProducts(params.category)
-  console.log(products);
 
   return (
-    <div>OrderPage</div>
+    <>
+      <div
+        className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start"
+      >
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
