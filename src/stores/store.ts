20c17a00e25e2d6e13e6d7d336216988db26e8a3
notlim_dev <elmotigre6@gmail.com>
2024-11-14 18:09:37 -0500

    Agregamos al estate de order  los platillos selecionados

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -6,18 +6,30 @@ interface Store {
   order: OrderItem[]
   addToCard: (product: Product) => void
 }
-export const useStoreQuiosco = create<Store>((set) => ({
+export const useStoreQuiosco = create<Store>((set, get) => ({
   order: [],
   addToCard: (product) => {
 
     const { categoryId, image, ...data } = product
-    set((state) => ({
+    let order: OrderItem[] = []
 
-      order: [...state.order, {
+    if (get().order.find(item => item.id === product.id)) {
+
+      order = get().order.map(item => item.id === product.id ? {
+        ...item,
+        quantity: item.quantity + 1,
+        subtotal: item.price * (item.quantity + 1)
+      } : item)
+    } else {
+
+      order = [...get().order, {
         ...data,
         quantity: 1,
         subtotal: 1 * product.price
       }]
+    }
+    set(() => ({
+      order
     }))
   }
 }))
