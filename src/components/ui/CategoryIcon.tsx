ce9d734247da527526bb92368b3ca1a8419f38ba
notlim_dev <elmotigre6@gmail.com>
2024-11-14 11:18:55 -0500

    Incorporando las Imagenes Dinamicamente en la Interfaz

--- src/components/ui/CategoryIcon.tsx
+++ src/components/ui/CategoryIcon.tsx
@@ -1,6 +1,27 @@
-export default function CategoryIcon() {
+import { Category } from "@prisma/client"
+import Image from "next/image"
+
+type CategoryIconProps = {
+  category: Category
+}
+export default function CategoryIcon({ category }: CategoryIconProps) {
   return (
-    <div>CategoryIcon</div>
+    <div
+      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3
+      last-of-type:border-b`}
+    >
+      <div className="w-16 h-16 relative">
+        <Image
+          fill
+          src={`/icon_${category.slug}.svg`}
+          alt="Imagen de Categoria"
+        />
+      </div>
+
+      <p
+        className="text-xl font-bold text-black"
+      >{category.name}</p>
+    </div>
   )
 }
 
