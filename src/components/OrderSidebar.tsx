716035ab1b2ff218e7db77617200ff33a667920e
notlim_dev <elmotigre6@gmail.com>
2024-11-14 10:58:04 -0500

    Creando, definiendo e instanciando prisma para data-fetching de categorias

--- src/components/OrderSidebar.tsx
+++ src/components/OrderSidebar.tsx
@@ -1,9 +1,23 @@
-export default function OrderSidebar() {
+import { prisma } from "@/lib/prisma"
+import CategoryIcon from "./ui/CategoryIcon"
+
+async function getCategorires() {
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+
+  const categorires = await getCategorires()
+
   return (
     <aside
       className="md:w-72 md:h-screen bg-white"
     >
-      OrderSidebar
+      <nav className="mt-10">
+        {categorires.map(category => (
+          <CategoryIcon />
+        ))}
+      </nav>
     </aside>
   )
 }
