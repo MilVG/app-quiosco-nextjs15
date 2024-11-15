47a42548ded501db74c510abc12d2357dc387590
notlim_dev <elmotigre6@gmail.com>
2024-11-15 11:28:51 -0500

    agregamos al objeto de datos, total y order

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -16,7 +16,9 @@ export default function OrderSummary() {
   const handleCreateOrder = async (formData: FormData) => {
 
     const data = {
-      name: formData.get('name')
+      name: formData.get('name'),
+      total,
+      order
     }
 
     const result = OrderSchema.safeParse(data)
