dfe29f9c52f0ddb5f52ed6339d48a1727d1cd0e7
notlim_dev <elmotigre6@gmail.com>
2024-11-14 19:00:09 -0500

    implementacion e incorporacion para calcular el total de las ordenes useMemo()

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -1,8 +1,14 @@
 "use client"
 import { useStoreQuiosco } from "@/stores/store"
 import ProductDetails from "./order/ProductDetails"
+import { useMemo } from "react"
+import { formatCurrency } from "@/utils"
 export default function OrderSummary() {
   const order = useStoreQuiosco((state) => state.order)
+
+  const total = useMemo(() => order.reduce(
+    (total, item) =>
+      total + (item.quantity * item.price), 0), [order])
   return (
     <aside
       className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5"
@@ -17,6 +23,11 @@ export default function OrderSummary() {
               item={item}
             />
           ))}
+
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar: {''}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
