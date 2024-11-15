cf28135d6d73038045d7bb437441340d16f21ea5
notlim_dev <elmotigre6@gmail.com>
2024-11-15 18:33:41 -0500

    Implementacion interfaz  listado de productos

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,9 +1,31 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
+
+  return products
+}
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
+
   return (
     <>
       <Heading>Administrar Productos</Heading>
+
+      <ProductTable
+        products={products}
+      />
     </>
   )
 }
