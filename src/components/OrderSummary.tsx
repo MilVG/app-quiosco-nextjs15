a6c0ba83878410daef48df0ad63262c25071d067
notlim_dev <elmotigre6@gmail.com>
2024-11-15 10:18:22 -0500

    Incorporacion de propiedad actions para  envio de datos de un state

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -9,6 +9,11 @@ export default function OrderSummary() {
   const total = useMemo(() => order.reduce(
     (total, item) =>
       total + (item.quantity * item.price), 0), [order])
+
+  const handleCreateOrder = () => {
+    console.log('desde el cliente');
+
+  }
   return (
     <aside
       className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5"
@@ -28,6 +33,19 @@ export default function OrderSummary() {
             Total a pagar: {''}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form
+            className="w-full mt-10 space-y-5"
+            action={handleCreateOrder}
+          >
+
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center
+              cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
