a044cdd9696bf8fc82a81384197f0ac4aeab1f41
notlim_dev <elmotigre6@gmail.com>
2024-11-15 12:21:00 -0500

    Finalizando capitulo 6 - Almcenando datos de las ordenes
    => utilizando server actions
    => utilziando toastify
    => utilziando zod

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -3,12 +3,44 @@ import { useStoreQuiosco } from "@/stores/store"
 import ProductDetails from "./order/ProductDetails"
 import { useMemo } from "react"
 import { formatCurrency } from "@/utils"
-export default function OrderSummary() {
-  const order = useStoreQuiosco((state) => state.order)
+import { createOrder } from "@/actions/create-order-action"
+import { OrderSchema } from "@/schema"
+import { toast } from "react-toastify"
 
+export default function OrderSummary() {
+
+  const order = useStoreQuiosco((state) => state.order)
+  const clearOrder = useStoreQuiosco((state) => state.clearOrder)
   const total = useMemo(() => order.reduce(
     (total, item) =>
       total + (item.quantity * item.price), 0), [order])
+
+  const handleCreateOrder = async (formData: FormData) => {
+
+    const data = {
+      name: formData.get('name'),
+      total,
+      order
+    }
+
+    const result = OrderSchema.safeParse(data)
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message)
+      })
+      return
+    }
+    const response = await createOrder(data)
+    if (response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message)
+      })
+      return
+    }
+
+    toast.success('Pedido Realizado Correctamente')
+    clearOrder()
+  }
   return (
     <aside
       className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5"
@@ -28,6 +60,25 @@ export default function OrderSummary() {
             Total a pagar: {''}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form
+            className="w-full mt-10 space-y-5"
+            action={handleCreateOrder}
+          >
+            <input
+              type="text"
+              placeholder="Tu Nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name"
+            />
+
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center
+              cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
