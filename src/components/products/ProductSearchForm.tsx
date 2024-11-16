12770666759f35610cf8cc6df82ae08fb634f59f
notlim_dev <elmotigre6@gmail.com>
2024-11-16 13:36:11 -0500

    Validacion para buscar productos

--- src/components/products/ProductSearchForm.tsx
+++ src/components/products/ProductSearchForm.tsx
@@ -1,6 +1,31 @@
+"use client"
+
+import { SearchSchema } from "@/schema"
+import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
+
 export default function ProductSearchForm() {
+
+  const router = useRouter()
+  const handleSearchForm = (formData: FormData) => {
+    const data = {
+      search: formData.get('search')
+    }
+    const result = SearchSchema.safeParse(data)
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      })
+
+      return
+    }
+    router.push(`/admin/products/search?search=${result.data.search}`)
+  }
   return (
-    <form className="flex items-center">
+    <form
+      className="flex items-center"
+      action={handleSearchForm}
+    >
 
       <input
         type="text"
