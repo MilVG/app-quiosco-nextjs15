8704e8a63e43b2b67766713b1044858a0649efd3
notlim_dev <elmotigre6@gmail.com>
2024-11-16 11:33:33 -0500

    Incorporacion de previus pagination

--- src/components/products/ProductsPagination.tsx
+++ src/components/products/ProductsPagination.tsx
@@ -9,6 +9,12 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
   return (
     <nav className="flex justify-center py-10">
 
+      {page > 1 && (
+        <Link
+          href={`/admin/products?page=${page - 1}`}
+        >&laquo;</Link>
+      )}
+
       {page < totalPages && (
         <Link
           href={`/admin/products?page=${page + 1}`}
