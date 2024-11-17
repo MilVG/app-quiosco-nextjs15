0725feac75c729ae64398cb9964b78dced1192d5
notlim_dev <elmotigre6@gmail.com>
2024-11-17 18:30:37 -0500

    Agregamos solucion para excluir elementos de un objecto con typescript

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -13,3 +13,14 @@ export function getImagePath(imagePath: string) {
     return `/products/${imagePath}.jpg`;
   }
 }
+
+export function excludeKeys<T, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
+  // Creamos un nuevo objeto excluyendo las claves especificadas
+  const result = { ...obj };
+
+  keys.forEach((key) => {
+    delete result[key]; // TypeScript sabe que 'key' es una clave válida de 'T'
+  });
+
+  return result;
+}
